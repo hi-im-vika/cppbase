7b18e6c0b2824e676518d4baa98f79d49f84857d
vika <37137412+hi-im-vika@users.noreply.github.com>
2025-03-20 09:01:44 -0700

    Add existing code from other projects

--- include/CBase.hpp
+++ include/CBase.hpp
@@ -3,3 +3,53 @@
  * 2025-03-20
  * vika <https://github.com/hi-im-vika>
  */
+
+#pragma once
+
+#include <thread>
+
+/**
+ * @brief A class designed to be inherited from to provide functions common to all
+ * @author vika
+ */
+class CBase {
+public:
+
+    /**
+     * @brief Constructor for CBase
+     */
+    CBase() = default;
+
+    /**
+     * @brief Destructor for CBase
+     */
+    ~CBase();
+
+    /**
+     * @brief Pure virtual function for updating app logic
+     */
+    virtual void update() = 0;
+
+    /**
+     * @brief Pure virtual function for updating UI
+     */
+    virtual void draw() = 0;
+
+    /**
+     * @brief Runs the update and draw functions.
+     */
+    void run();
+
+    /**
+     * @brief Thread for updating app logic
+     * @param who_called_me Pointer to CBase object of interest
+     */
+    static void update_thread(CBase *who_called_me);
+
+protected:
+    std::chrono::steady_clock::time_point _perf_update_start;       ///< Start time for measuring performance.
+    std::chrono::steady_clock::time_point _perf_draw_start;         ///< Start time for measuring performance.
+    int _perf_update = 1;       ///< Measured update time in milliseconds.
+    int _perf_draw = 1;         ///< Measured draw time in milliseconds.
+    bool _do_exit = false;      ///< Flag to exit program.
+};
--- src/CBase.cpp
+++ src/CBase.cpp
@@ -5,3 +5,25 @@
  */
 
 #include "../include/CBase.hpp"
+
+CBase::~CBase() = default;
+
+void CBase::run() {
+    // start update thread
+    std::thread thread_for_updating(update_thread, this);
+    thread_for_updating.detach();
+    // draw UI in main thread
+    do draw();
+    while (!_do_exit);
+    // handle exit
+    if (thread_for_updating.joinable()) thread_for_updating.join();
+}
+
+void CBase::update_thread(CBase *who_called_me) {
+    while (!(who_called_me->_do_exit)) {
+        who_called_me->_perf_update_start = std::chrono::steady_clock::now();
+        who_called_me->update();
+        who_called_me->_perf_update = (int) std::chrono::duration_cast<std::chrono::milliseconds>(
+                std::chrono::steady_clock::now() - who_called_me->_perf_update_start).count();
+    }
+}
