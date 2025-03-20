d3be11b99684c36d00fb2758620f987f79182dc1
vika <37137412+hi-im-vika@users.noreply.github.com>
2025-03-20 09:32:45 -0700

    Add tester

--- test/TestCBase.cpp
+++ test/TestCBase.cpp
@@ -3,3 +3,28 @@
  * 2025-03-20
  * vika <https://github.com/hi-im-vika>
  */
+
+#include "TestCBase.hpp"
+
+TestCBase::TestCBase() {
+    _text = "Hello world!";
+    _count = 0;
+}
+
+TestCBase::~TestCBase() = default;
+
+void TestCBase::update() {
+    _count++;
+}
+
+void TestCBase::draw() {
+    std::cout << _text << std::endl;
+    std::cout << "Current count: " << _count << std::endl;
+    std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::milliseconds(1000));
+}
+
+int main() {
+    TestCBase c = TestCBase();
+    c.run();
+    return 0;
+}
